db4b86c61968e21cd76f50ce05d2e4254f4e7860
Omkar Bhoir <71968255+omkar787@users.noreply.github.com>
2021-09-03 12:29:07 +0530

    You can find you robot now

--- src/components/Container.js
+++ src/components/Container.js
@@ -10,7 +10,15 @@ export default function Container() {
 
     function onClickHandle(e){
         const temp = searchBox.current.value
-        setsearch(temp)
+        console.log(temp);
+        if(temp === ""){
+            setsearch("Harry")
+        }else{
+            setsearch(temp)
+        }
+
+        searchBox.current.value = ""
+        
     }
 
     function onChangeHandle(e){
@@ -42,15 +50,16 @@ export default function Container() {
         <div className="container">
             <h1 className="heading">Is this you?</h1>
             <div>
-            <input ref={searchBox} type="text" placeholder="Harry"/>
-            <select onChange={onChangeHandle}>
+            <input className="search-box" ref={searchBox} type="text" placeholder={search}/>
+            <select className="drop-down" onChange={onChangeHandle}>
                 <option value="1">Robots</option>
                 <option value="2">Monsters</option>
                 <option value="3">Only heads</option>
                 <option value="4">Cute cats</option>
             </select>
+            <button className="btn" onClick={onClickHandle}>Revelio</button>
             </div>
-            <button onClick={onClickHandle}>Revelio</button>
+            
             <img src={url + search + "?set=" + setItem} alt={search} />
         </div>
     )
